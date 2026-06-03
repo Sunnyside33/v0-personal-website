5b386539c1aa7cd8c4eed9445a74e68a44abefc3
Sunnyside33 <bangkit.suryatamaa@gmail.com>
2026-06-03 18:22:25 +0700

    Create icon.tsx

---
+++ app/icon.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: 32,
+          height: 32,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+          background: "#FAFAFA",
+        }}
+      >
+        <img
+          src="/telor.png"
+          width={32}
+          height={32}
+          style={{ borderRadius: "50%" }}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
